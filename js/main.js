f7b9e159c20ca906ac7047ad981f81f1ea99e7c2
Гусейнов Ровшан Муса Оглы <guseynov_rovshan_2016@MacBook-Air-Gusejnov.local>
2022-05-23 05:21:53 +0300

    minor up

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const regForm = document.getElementById('regForm');
+const regStatus = document.getElementById('regStatus');
+
 // Scroll to anchors
 (function () {
 
@@ -53,3 +56,25 @@ form.addEventListener("submit", function(event) {
   log.innerText = output;
   event.preventDefault();
 }, false);
+
+
+// Запрос
+regForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    regStatus.classList.remove('warning');
+    const data = Object.fromEntries(new FormData(event.target).entries());
+    const req = new XMLHttpRequest();
+    req.open('POST', 'http://localhost:3000/regform');
+    req.responseType = "json";
+    req.setRequestHeader('Content-Type', 'application/json');
+
+    req.onreadystatechange = function() {
+        if (this.readyState != 4) return;
+        console.log(typeof this.response)
+        if(this.response.error){
+            regStatus.classList.add('warning');
+        }
+        regStatus.innerText = this.response.message;
+    }
+    req.send(JSON.stringify(data));		
+})
